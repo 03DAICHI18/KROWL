96f63b48617d6a5ed983562df1ca6f37f1eebf10
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-08-27 13:31:14 +0900

    ページ内スムーススクロール

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ $(function () {
   $('.js-btn').on('click', function () { // js-btnクラスをクリックすると、
     $('.header__gnav , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
   })
+
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    var speed = 500;
+    $("html, body").animate({
+      scrollTop: position - 80
+    }, speed, "swing");
+    return false;
+  });
 })
